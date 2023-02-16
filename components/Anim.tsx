69f54503da09d9c0a31e5079f2bddcdac09eabfb
jessej-samuel <1234jessej@gmail.com>
2023-02-16 20:47:42 +0530

    bruh the animation works! (mobile res)

--- components/Anim.tsx
+++ components/Anim.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import styles from "./Anim.module.css";
+import Image from "next/image";
+import res from "../public/assets/res.png";
+import img2 from "../public/assets/img2.png";
+import img1 from "../public/assets/img1.png";
 
 function Anim() {
-    return (
-        <div>Anim</div>
-    )
+  return (
+    <div className={styles.animContainer}>
+      <div className={styles.animationContainer}>
+        <Image
+          className={styles.img_1}
+          src={img1.src}
+          width={150}
+          height={150}
+          alt={""} // Must be updated from image description
+        />
+        <Image
+          className={styles.img_res}
+          src={res.src}
+          width={150}
+          height={150}
+          alt={""} // Must be updated from image description
+        />
+        <Image
+          className={styles.img_2}
+          src={img2.src}
+          width={150}
+          height={150}
+          alt={""} // Must be updated from image description
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Anim
+export default Anim;
