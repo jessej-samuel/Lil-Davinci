52a0ab3914ec3187445f477a0b335e85fac528d3
jessej-samuel <1234jessej@gmail.com>
2023-02-21 20:20:51 +0530

    add: animations

--- components/Anim.tsx
+++ components/Anim.tsx
@@ -8,28 +8,39 @@ import img1 from "../public/assets/img1.png";
 function Anim() {
   return (
     <div className={styles.animContainer}>
-      <div className={styles.animationContainer}>
-        <Image
-          className={styles.img_1}
-          src={img1.src}
-          width={150}
-          height={150}
-          alt={""} // Must be updated from image description
-        />
-        <Image
-          className={styles.img_res}
-          src={res.src}
-          width={150}
-          height={150}
-          alt={""} // Must be updated from image description
-        />
-        <Image
-          className={styles.img_2}
-          src={img2.src}
-          width={150}
-          height={150}
-          alt={""} // Must be updated from image description
-        />
+      <div style={{ background: "yellow" }} className={styles.viewHolder}>
+        <div className={styles.animationContainer}>
+          <Image
+            className={styles.img_1}
+            src={img1.src}
+            width={150}
+            height={150}
+            alt={""} // Must be updated from image description
+          />
+          <Image
+            className={styles.img_res}
+            src={res.src}
+            width={150}
+            height={150}
+            alt={""} // Must be updated from image description
+          />
+          <Image
+            className={styles.img_2}
+            src={img2.src}
+            width={150}
+            height={150}
+            alt={""} // Must be updated from image description
+          />
+        </div>
+      </div>
+      <div className={styles.description}>
+        <h2>Createart</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus
+          sit, consectetur officiis, totam distinctio modi provident voluptates
+          ex quod amet quasi maiores molestias dolore voluptate, illo fuga
+          laudantium. Impedit, explicabo?
+        </p>
       </div>
     </div>
   );
